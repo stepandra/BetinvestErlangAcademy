e8323d66e7f94fd8c91d06046e2a826cd694888d
Stepan Mytchyk <stepandra94@gmail.com>
2015-06-01 19:14:18 +0300

    Update p08.erl

--- p08.erl
+++ p08.erl
@@ -1,12 +1,16 @@
 -module(p08).
 -export([compress/1]).
 
+
 compress([])->
 	[];
+	
 compress([X|[X|T]])->
 	compress([X|T]);
+	
 compress([X|[H|T]])->
 	[X|compress([H|T])];
+	
 compress([X|[]])->
 	[X].
 
